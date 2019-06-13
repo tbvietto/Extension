0cbf47bf61db4caa5bf164db54428a89509fb368
tbvietto <tbv.bkhn@gmail.com>
2019-06-14 03:03:07 +0900

    make add slack webhook url feature

--- popup.js
+++ popup.js
@@ -1,3 +1,34 @@
+function openSettingsSlack() {
+  document.getElementById('settings-slack').classList.toggle('settings-open');
+}
+
+document.getElementById('settings-slack-button').addEventListener('click', openSettingsSlack);
+
+
+document.getElementById('set-webhook-url').addEventListener('submit',function (e) {
+  e.preventDefault();
+  getURL();
+});
+
+function getURL() {
+  webhookUrl = document.getElementById('url-input').value;
+  saveWebhookUrl();
+}
+
+function saveWebhookUrl() {
+  localStorage.setItem('receivedUrl', webhookUrl);
+}
+
+var webhookUrl = localStorage.getItem('receivedUrl');
+
+if (webhookUrl == null) {
+  webhookUrl = "https://hooks.slack.com/services/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";
+}
+
+console.log(localStorage.getItem('receivedUrl'));
+console.log(webhookUrl);
+
+
 function saveQuestion() {
   localStorage.setItem('receivedSentence', sentence);
   localStorage.setItem('receivedWord', word);
@@ -23,7 +54,6 @@ function showQuestion() {
   document.getElementById("show-question").innerHTML = `${sentence} ${word}`;
 }
 
-const webhookUrl = "https://hooks.slack.com/services/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";
 function sendToSlack(word, mean) {
   $.ajax({
     data: 'payload=' + JSON.stringify({
@@ -32,6 +62,6 @@ function sendToSlack(word, mean) {
     dataType: 'json',
     processData: false,
     type: 'POST',
-    url: webhookUrl
+    url: webhookUrl,
   });
 }
